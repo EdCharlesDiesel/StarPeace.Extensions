f8a9975af22706ca6d2e01d1f8e4c0eb47a37535
Khotso Mokhethi <Mokhetkc@hotmail.com>
2022-04-01 00:26:46 +0200

    Initial commit

---
+++ StarPeace.Extension/IndexMultipliedBy5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StarPeace.Extension
+{
+    public static class IndexMultiplied
+    {
+        public static int[] MultplyByFive(this Array[] arrays)
+        {
+            int[] arr = new int[20];
+ 
+        	for (int i = 0; i <arr.Length; i ++)
+        	{
+            	arr [i] = i * 5;
+        	}
+ 
+        	for (int i = 0; i <arr.Length; i ++)
+        	{
+            	Console.WriteLine ("element {0} = {1}", i + 1, arr [i]);
+        	}
+            return arr;
+    	   // Console.ReadLine();
+        }
+    }
+}
